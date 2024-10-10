a2723b7af7a4bec44280c53d4d453bd2ac06e334
jacobalmon <jacobalmon03@gmail.com>
2024-10-10 09:13:25 -0700

    update client

--- Project/src/myapp/Client.java
+++ Project/src/myapp/Client.java
@@ -1,5 +1,28 @@
 package myapp;
 
-public class Client {
+import java.io.*;
+import java.net.*;
 
+public class Client {
+	// Initializing Socket and Input & Output Streams.
+	private Socket socket = null;
+	private DataInputStream input = null;
+	private DataOutputStream output = null;
+	
+	public Client(String address, int port) {
+		try {
+			socket = new Socket(address, port);
+			System.out.println("Connected");
+			
+			
+		} catch (UnknownHostException u) {
+			return;
+		} catch (IOException i) {
+			return;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Client client = new Client("127.0.0.1", 5000);
+	}
 }
