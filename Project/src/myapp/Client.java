d215103b07a3a81dde47f2e8aff063f3c6da0e39
jacobalmon <jacobalmon03@gmail.com>
2024-10-29 09:09:13 -0700

    skeleton for register

--- Project/src/myapp/Client.java
+++ Project/src/myapp/Client.java
@@ -31,6 +31,9 @@ public class Client extends Application {
 		Button loginButton = new Button("Login");
 		Label statusLabel = new Label();
 		
+		// Register Button.'
+		Button registerButton = new Button("Register");
+		
 		// Login Button Event Listener.
 		loginButton.setOnAction(e -> {
 			String username = usernameField.getText();
@@ -46,8 +49,13 @@ public class Client extends Application {
 			}
 		});
 		
+		// Register Button Event Listener.
+		registerButton.setOnAction(e -> {
+			showRegisterPage();
+		});
+		
 		// Screen for Login Page.
-		VBox layout = new VBox(10, usernameField, passwordField, loginButton, statusLabel);
+		VBox layout = new VBox(10, usernameField, passwordField, loginButton, statusLabel, registerButton);
 		Scene loginScene = new Scene(layout, 300, 200);
 		
 		// Show Login Page.
@@ -57,7 +65,45 @@ public class Client extends Application {
 	}
 	
 	private void showRegisterPage() {
-		// TBA.
+		// Textbox for First Name.
+		TextField firstNameText = new TextField();
+		firstNameText.setPromptText("First Name");
+		
+		// Textbox for Last Name.
+		TextField lastNameText = new TextField();
+		lastNameText.setPromptText("Last Name");
+		
+		// Textbox for Username.
+		TextField usernameText = new TextField();
+		usernameText.setPromptText("Username");
+		
+		// Textbox for Email.
+		TextField emailText = new TextField();
+		emailText.setPromptText("Email");
+		
+		// Textbox for Password.
+		TextField passwordText = new TextField();
+		passwordText.setPromptText("Password");
+		
+		// Textbox for Confirm Password.
+		TextField confirmPasswordText = new TextField();
+		confirmPasswordText.setPromptText("Confirm Password");
+		
+		// Button for Create Account.
+		Button createAccountButton = new Button("Create Account");
+		
+		createAccountButton.setOnAction(e -> {
+			
+		});
+		
+		// Screen for Creating a new Account.
+		VBox layout = new VBox(10, firstName, lastName, username, email, password, confirmPassword, createAccount);
+		Scene registerScene = new Scene(layout, 300, 200);
+		
+		primary.setScene(registerScene);
+		primary.setTitle("Create New Account");
+		primary.show();
+		
 	}
 	
 	private boolean sendLoginRequest(String username, String password) {
