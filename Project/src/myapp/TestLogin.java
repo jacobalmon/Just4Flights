6e82d2c26a39ea3dabd414d4cfc721cb588e8cde
jacobalmon <jacobalmon03@gmail.com>
2024-11-06 09:04:03 -0800

    add junit file

---
+++ Project/src/myapp/TestLogin.java
@@ -0,0 +1,30 @@
+package myapp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestLogin {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {}
+
+	@BeforeEach
+	void setUp() throws Exception {}
+
+	@AfterEach
+	void tearDown() throws Exception {}
+	
+	@Test
+	void existingUser() {
+		
+	}
+
+}
