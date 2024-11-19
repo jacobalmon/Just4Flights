6a1b590b9074b87028d325ac99645f0fb930c762
jacobalmon <jacobalmon03@gmail.com>
2024-11-19 09:40:15 -0800

    api code done;

--- Project/src/test/TestAPI.java
+++ Project/src/test/TestAPI.java
@@ -6,29 +6,29 @@ import myapp.SearchFlights;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestAPI {
-//	@Test
-//	public void Airport1() throws UnirestException {
-//		String loc = "Seattle";
-//		String response = SearchFlights.getAirport(loc);
-//		String expected = "SkyId: SEAA, EntityId: 27538444";
-//		assertEquals(response, expected, "Id's should match.");
-//	}
-//	
-//	@Test
-//	public void Airport2() throws UnirestException {
-//		String loc = "San Diego";
-//		String response = SearchFlights.getAirport(loc);
-//		String expected = "SkyId: SANA, EntityId: 27545066";
-//		assertEquals(response, expected, "Id's should match.");
-//	}
-//	
-//	@Test
-//	public void Airport3() throws UnirestException {
-//		String loc = "New York City";
-//		String response = SearchFlights.getAirport(loc);
-//		String expected = "SkyId: NYCA, EntityId: 27537542";
-//		assertEquals(response, expected, "Id's should match.");
-//	}
+	@Test
+	public void Airport1() throws UnirestException {
+		String loc = "Seattle";
+		String response = SearchFlights.getAirport(loc);
+		String expected = "SkyId: SEAA, EntityId: 27538444";
+		assertEquals(response, expected, "Id's should match.");
+	}
+	
+	@Test
+	public void Airport2() throws UnirestException {
+		String loc = "San Diego";
+		String response = SearchFlights.getAirport(loc);
+		String expected = "SkyId: SANA, EntityId: 27545066";
+		assertEquals(response, expected, "Id's should match.");
+	}
+	
+	@Test
+	public void Airport3() throws UnirestException {
+		String loc = "New York City";
+		String response = SearchFlights.getAirport(loc);
+		String expected = "SkyId: NYCA, EntityId: 27537542";
+		assertEquals(response, expected, "Id's should match.");
+	}
 	
 	@Test
 	public void Flight1() throws UnirestException {
